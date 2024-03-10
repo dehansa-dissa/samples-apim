e6a2a431fe73ac52ba6e05c85bf97cba81d56843
arshardh <arshardhifthikar50@gmail.com>
2024-03-11 00:27:51 +0530

    Add simple python API

---
+++ marketplace-assistant-api/app/api.py
@@ -0,0 +1,26 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+api = FastAPI(
+    title="API Marketplace Chatbot",
+    version="0.1.0",
+)
+
+class Query(BaseModel):
+    query: str
+
+@api.post("/chat")
+async def chat(query: Query):
+    return {'query': query}
