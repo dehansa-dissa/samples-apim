89faf59848b05e3fd5c898c80fade463e38ba7ca
arshardh <arshardhifthikar50@gmail.com>
2024-03-21 00:28:26 +0530

    Add spec population service

---
+++ spec_populator_service/main.py
@@ -0,0 +1,54 @@
+from milvus import *
+from fastapi import FastAPI
+from utils import *
+import asyncio
+from functools import partial
+
+embed = get_emb_model()
+source = os.getenv("SOURCE_PLATFORM")
+
+app = FastAPI()
+
+@app.post("/add_vector/{uuid}")
+async def add_vector(uuid : str, req: Dict[str, Any]):
+
+    # Introspect token and get vector or user
+    collection = "common_space"
+
+    if source == "apim":
+        api_type = req["api_type"]
+        match api_type:
+            case "REST":
+                record = await pre_process_openapi(req["api_spec"])
+            case "GRAPHQL":
+                record = await pre_process_graphql_sdl(req["sdl_schema"])
+            case "ASYNC":
+                record = await pre_process_asyncapi_def(req["async_spec"])
+        
+        # Add API type, available subscription plans
+        record["api_type"] = api_type
+        # The actual version is used instead of what is in the Spec,
+        # since we know this is the truth, and the spec version can be outdated
+        record["version"] = req["version"]
+        record["api_name"] = req["api_name"]
+        record["apim_description"] = req["description"]
+
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(None, partial(upsert_vector_for_onprem, embed, record, collection, uuid, api_type, req["api_name"], req["tenant_domain"]))
+    elif source == "choreo":
+        record = await pre_process_openapi(req["api_spec"])
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(None,partial(upsert_vector_for_choreo, embed, record, collection, uuid))
+
+    return {"message": response}
+
+@app.delete("/remove_vector/{uuid}")
+async def remove_vector(uuid : str):
+    
+    collection = "common_space"
+
+    if source == "apim":
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(None, partial(delete_vector_for_onprem, [uuid], collection))
+
+    return {"message": response}
---
+++ spec_populator_service/milvus.py
@@ -0,0 +1,106 @@
+from pymilvus import DataType, MilvusClient
+import os
+from utils import *
+
+api_key = os.getenv('MILVERSE_API_KEY')
+url = os.getenv('MILVERSE_URL')
+
+def upsert_vector_for_onprem(embed, record, collection, api_id, api_type, api_name, tenant):
+
+    mc = MilvusClient(uri=url, token=api_key)
+    collection_name = collection + "__apim__"
+
+    has = mc.has_collection(collection_name)
+    if not has:
+        schema = MilvusClient.create_schema(
+            auto_id=False,
+            enable_dynamic_field=True,
+        )
+        schema.add_field(field_name="id", datatype=DataType.VARCHAR, is_primary=True, max_length=100)
+        schema.add_field(field_name="api_name", datatype=DataType.VARCHAR, max_length=512)
+        schema.add_field(field_name="api_type", datatype=DataType.VARCHAR, max_length=100)
+        schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=1536)
+        schema.add_field(field_name="page_content", datatype=DataType.VARCHAR, max_length=10000)
+        schema.add_field(field_name="tenant_domain", datatype=DataType.VARCHAR, max_length=512,  is_partition_key=True)
+
+        index_params = mc.prepare_index_params()
+
+        index_params.add_index(
+            field_name="vector", 
+            index_type="AUTOINDEX",
+            metric_type="L2"
+        )
+        mc.create_collection(
+            collection_name=collection_name,
+            metric_type="COSINE",
+            schema=schema,
+            index_params=index_params
+        )
+
+    res = embed.embed_query(str(record))
+    payload={
+            "page_content": str(record),
+            "api_name": api_name,
+            "id": api_id,
+            "vector": res,
+            "api_type": api_type,
+            "tenant_domain": tenant
+        }
+
+    response = mc.upsert(collection_name=collection_name, data=payload)
+    return response
+
+
+def upsert_vector_for_choreo(embed, record, collection, api_id, api_name):
+
+    mc = MilvusClient(uri=url, token=url)
+    collection_name = collection + "__choreo__"
+
+    has = mc.has_collection(collection_name)
+    if not has:
+
+        schema = MilvusClient.create_schema(
+            auto_id=False,
+            enable_dynamic_field=True,
+        )
+        schema.add_field(field_name="id", datatype=DataType.VARCHAR, is_primary=True, max_length=100)
+        schema.add_field(field_name="api_name", datatype=DataType.VARCHAR, max_length=512)
+        schema.add_field(field_name="api_type", datatype=DataType.VARCHAR, max_length=100)
+        schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=1536)
+        schema.add_field(field_name="page_content", datatype=DataType.VARCHAR, max_length=10000)
+
+        index_params = mc.prepare_index_params()
+
+        index_params.add_index(
+            field_name="vector", 
+            index_type="AUTOINDEX",
+            metric_type="L2"
+        )
+
+        mc.create_collection(
+            collection_name=collection_name,
+            metric_type="COSINE",
+            schema=schema,
+            index_params=index_params
+        )
+        
+    res = embed.embed_query(str(record))
+    payload={
+            "page_content": str(record),
+            "api_name": api_name,
+            "id": api_id,
+            "vector": res
+        }
+    response = mc.upsert(collection_name=collection_name, data=payload)
+    return response
+
+def delete_vector_for_onprem(uuid, collection):
+
+    collection = collection + "__apim__"
+    mc = MilvusClient(uri=url, token=api_key)
+
+    res = mc.delete(
+        collection_name=collection,
+        ids=uuid
+    )
+    return res
