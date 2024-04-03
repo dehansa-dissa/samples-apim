bcc2501b8355ddbfa777b404611d64fcc1ffd2ac
isuruRuhu <isuru.ruhu@gmail.com>
2024-04-03 14:35:12 +0530

    Intergrate assistant changes to choreo

--- marketplace-assistant-api/app/api.py
+++ marketplace-assistant-api/app/api.py
@@ -207,7 +207,7 @@ def prepare_rag_chain(tenant_domain: str, partition_id: str, stream=False):
             Context: {context}"""
         else:
             qa_system_prompt = """System: You are a simple, and cheerful API Marketplace assistant. Who only speak correct markdown text. Based on the provided API details, 
-            recommend relevant APIs. Ensure the recommendation is accurate and tailored to the user's needs. If you can't find the API from the context, just say that you don't know politely.
+            recommend all relevant APIs. Ensure the recommendation is accurate and tailored to the user's needs. If you can't find the API from the context, just say that you are not aware of such an API politely.
             Understand the provided context and IGNORE the APIs that does not match the human question. DO NOT SHOW ANY URLS including REDIRECT URLS in the response. 
             Please note that the context contains information about different types of APIs: REST, GraphQL, and Async.
             Only recommend APIs that are specified in the context and avoid including made-up APIs.
