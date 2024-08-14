ec652256bed8712fcf89f0c484869ae05504c2c8
isuruRuhu <isuru.ruhu@gmail.com>
2024-08-14 17:36:05 +0530

    Address the comments

--- spec_populator_service/main.py
+++ spec_populator_service/main.py
@@ -27,10 +27,10 @@ excluded_org_list = os.getenv(const.EXCLUDED_ORG_LIST, "").split(",")
 app = FastAPI()
 
 # Setting log levels
-loglevel = os.getenv('LOGLEVEL', 'INFO')
-logging.basicConfig(level=loglevel)
+log_level = os.getenv('LOGLEVEL', logging.INFO)
+logging.basicConfig(level=log_level)
 for logger_name in logging.root.manager.loggerDict:
-    logging.getLogger(logger_name).setLevel(loglevel)
+    logging.getLogger(logger_name).setLevel(log_level)
 
 logging.getLogger("uvicorn.access").addFilter(EndpointFilter(const.EXCLUDED_ENDPOINTS))
 
