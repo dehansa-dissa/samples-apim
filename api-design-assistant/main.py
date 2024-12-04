6ee6a323cef8d3d17465ff2aa28c3cd3d69c3609
KamalikaSene <kamalikasene@gmail.com>
2024-12-04 08:29:41 +0530

    Add license headers to source files and delete main.py

--- api-design-assistant/main.py
+++
@@ -1,6 +0,0 @@
-from methods import query_llm
-
-if __name__ == "__main__":
-    while True:
-        user_input = input("Enter your API use case or type a synonym of 'modify' to include modifications to the code: ")
-        query_llm(user_input)
