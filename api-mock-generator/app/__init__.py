6e3206eb3ce7427a9097d51cdb605efb190b8212
anushanga <anushanga@wso2.com>
2025-05-06 12:04:52 +0530

    Remove unused app initialization and run script; update requirements for FastAPI standard

--- api-mock-generator/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from config.settings import Config
-from app.routes import api_router
-
-def create_app():
-    app = FastAPI()
-    # If Config has relevant settings, they can be applied here as needed
-    app.include_router(api_router)
-    return app
